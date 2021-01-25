26ef6413eff77676a4a629cc14bb74514945e8f6
ducnhat <ducnhat090199@gmail.com>
2021-01-25 10:03:38 +0700

    tách Areas

---
+++ ManagerProject/Areas/Manager/Controllers/ManagerHomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ManagerProject.Areas.Manager.Controllers
+{
+    public class ManagerHomeController : Controller
+    {
+        // GET: Manager/ManagerHome
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ManagerProject/Areas/Manager/ManagerAreaRegistration.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ManagerProject.Areas.Manager
+{
+    public class ManagerAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Manager";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Manager_default",
+                "Manager/{controller}/{action}/{id}",
+                new { action = "Index", Controller = "ManagerHome", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
